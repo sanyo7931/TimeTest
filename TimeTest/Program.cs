56afd30f814cbbe16ca32dcc4c83d0583357a73f
sanyo7931 <sanyo7931@yahoo.com.tw>
2020-01-08 21:34:20 +0800

    測試

---
+++ TimeTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入24時制時間: ");
+            string militaryTime = Console.ReadLine();
+
+            Regex regex = new Regex(@"^([0-1][0-9]|[2][0-3])\:[0-5][0-9]$");
+            bool result = regex.IsMatch(militaryTime);
+            try
+            {
+                if (result)
+                {
+                    Console.WriteLine(militaryTime);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
